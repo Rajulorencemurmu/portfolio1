b21d13fd8b364a40d9cccc9aba23b5bb4c770bf9
Raju Lorence <101400321+Rajulorencemurmu@users.noreply.github.com>
2023-03-08 12:59:24 +0530

    Update About.jsx

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, setAbouts } from "react";
 // import { motion } from 'framer-motion';
 
 import { AppWrap, MotionWrap } from "../../wrapper";
